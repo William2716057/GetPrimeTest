c02665efcf1425e0574a1a76906d2916a98ee0e6
William Spriggs <Willspri@gmail.com>
2023-11-26 16:18:19 +1300

    comments added

--- GetPrimeTest/GetPrimeTest.py
+++ GetPrimeTest/GetPrimeTest.py
@@ -2,7 +2,9 @@ from Crypto.Util.number import getPrime
 import time
 
 seenNumbers = set()
+#A larger bit size will result in less likelyhood of finding a repeat in a feasible amount of time
 bitSize = 1024
+#initialise the attempts at zero to display the results later
 attempts = 0
 
 start = time.time()
